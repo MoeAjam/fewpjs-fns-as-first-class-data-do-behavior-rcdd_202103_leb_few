b3bf6d722e4717d82345da2a5ab07f51dcfbe6dd
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-13 13:29:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(t) {
+  t = t.split(:);
+  t = t.parseInt();
+  if(t<12){
+    return "Good Morning";
+  } else if (t>17) {
+    return "Good Evening";
+  } else {
+    return "Good Afternoon";
+  }
+}
 /* Write your implementation of displayMessage() */
