0e9461af73f4ce5c3d52d586ed9a79ffdfe253f2
Mahmoud Ajam <mahmoud.aajam@gmail.com>
2021-04-13 13:42:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ function greet(t) {
 }
 /* Write your implementation of displayMessage() */
 function displayMessage(s) {
-  document.getElementById(#greeting).textContent = s;
+  document.getElementById(#greeting).innerHTML = s;
 }
